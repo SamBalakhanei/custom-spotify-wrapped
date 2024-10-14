9f3f55207ac7677750e278a559b4b509ba4eec19
Sam <thesamloop@gmail.com>
2024-10-14 16:46:40 -0400

    iniitalize index.html

--- custom_wrapped/app/views.py
+++ custom_wrapped/app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 def index(request):
-    return HttpResponse("Test test test");
+    template_name = 'index.html'
+    return render(request, template_name)
 
 
