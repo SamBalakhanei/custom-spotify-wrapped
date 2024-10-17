55a77f12f019340ff7ab263d785b77f44c3c8b24
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2024-10-17 19:27:59 -0400

    Added endpoint to get top artists

--- custom_wrapped/app/urls.py
+++ custom_wrapped/app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('spotify/login/', views.spotify_login, name='spotify_login'),
     path('spotify/callback/', views.spotify_callback, name='spotify_callback'),
     path('logout/', views.logout, name='logout'),
-    path('spotify/get_top_tracks/<int:limit>/<str:period>/', views.get_top_tracks, name='spotify_get_top_tracks')
+    path('spotify/get_top_tracks/<int:limit>/<str:period>/', views.get_top_tracks, name='spotify_get_top_tracks'),
+    path('spotify/get_top_artists/<int:limit>/<str:period>/', views.get_top_artists, name='spotify_get_top_artists')
 
 ]
--- custom_wrapped/app/views.py
+++ custom_wrapped/app/views.py
@@ -89,7 +89,6 @@ def get_spotify_user_profile(access_token):
 def get_top_tracks(request, limit, period):
     endpoint = 'https://api.spotify.com/v1/me/top/tracks'
 
-
     try:
         headers = {
             'Authorization': f'Bearer {request.session['spotify_access_token'] }',
@@ -104,7 +103,7 @@ def get_top_tracks(request, limit, period):
         
         data = response.json()
         top_tracks = {}
-        numTracks = 1
+        num_tracks = 1
         
         for datum in data["items"]:   
             artists = []
@@ -114,7 +113,7 @@ def get_top_tracks(request, limit, period):
                     "artist_link": artist["href"]
                 })  
                                   
-            top_tracks[numTracks] = {
+            top_tracks[num_tracks] = {
                 "track_name" : datum["name"],
                 "mp3_preview_url": datum["preview_url"],
                 "track_url" : datum["external_urls"]["spotify"],
@@ -126,7 +125,7 @@ def get_top_tracks(request, limit, period):
                 "release_date": datum["album"]["release_date"],
             }
             
-            numTracks += 1
+            num_tracks += 1
             
         return JsonResponse(top_tracks, status=200)
     except Exception as e:
@@ -136,4 +135,43 @@ def get_top_tracks(request, limit, period):
         }
             
         return JsonResponse(error_data, status=500)
+
+def get_top_artists(request, limit, period):
+    endpoint = 'https://api.spotify.com/v1/me/top/artists'
+
+    try:
+        headers = {
+            'Authorization': f'Bearer {request.session['spotify_access_token'] }',
+        }
+        
+        params = {
+            "limit": limit,
+            "time_range" : period
+        }
+        
+        response = requests.get(endpoint, headers=headers, params=params)
+        
+        data = response.json()
+        top_artists = {}
+        num_artists = 1
+        
+        for datum in data["items"]:                    
+            top_artists[num_artists] = {
+                "name": datum["name"],
+                "artist_url": datum["external_urls"]["spotify"],
+                "genres": datum["genres"],
+                "image_url": datum["images"][0]["url"]
+            }
+            
+            num_artists += 1
+            
+        return JsonResponse(top_artists, status=200)
+    except Exception as e:
+        error_data = {
+            "message": "Operation failed",
+            "error": str(e)
+        }
+            
+        return JsonResponse(error_data, status=500)
+
 
