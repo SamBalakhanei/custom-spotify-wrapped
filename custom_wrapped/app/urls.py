8d6e77db7c3b33767a7282c74617e747c1d62e64
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2024-10-17 19:13:40 -0400

    created endpoint to get top tracks w/ custom params

--- custom_wrapped/app/urls.py
+++ custom_wrapped/app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('spotify/login/', views.spotify_login, name='spotify_login'),
     path('spotify/callback/', views.spotify_callback, name='spotify_callback'),
     path('logout/', views.logout, name='logout'),
+    path('spotify/get_top_tracks/<int:limit>/<str:period>/', views.get_top_tracks, name='spotify_get_top_tracks')
 
 ]
