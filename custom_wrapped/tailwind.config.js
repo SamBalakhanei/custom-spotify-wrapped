f89d22d27a7f16afd6cfd72c023c42faed967356
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2024-10-21 13:24:37 -0400

    styled the welcome and login page

--- custom_wrapped/tailwind.config.js
+++ custom_wrapped/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
       './templates/**/*.html'
   ],
   theme: {
+    colors: {
+      'black': '#191414',
+      'green': '#1DB954',
+      'dark-gray': '#121212',
+      'light-gray': '#B3B3B3',
+      'white': '#FFFFFF',
+    },
+    container: {
+      center: true,
+    },
     extend: {},
   },
   plugins: [],
