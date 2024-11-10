c905905d745f89ff4bb1c27e745ef234e8979873
Jonathan Jia <cheekysquid@lawn-10-91-11-208.lawn.gatech.edu>
2024-11-09 21:47:36 -0500

    Added new features to wrapped, combined tracks + artists, made wrapped.html use 'include'

--- custom_wrapped/app/spotify_api.py
+++ custom_wrapped/app/spotify_api.py
@@ -128,3 +128,46 @@ def get_top_artists(access_token, limit=10, time_range='medium_term'):
         return response.json()
     else:
         response.raise_for_status()
+
+
+def get_track_features(access_token, track_id):
+    url = f'{SPOTIFY_API_BASE_URL}/audio-features/{track_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def get_related_artists(access_token, artist_id, limit=5):
+    """
+    Retrieves related artists for a given artist ID.
+
+    Args:
+        access_token (str): Spotify API access token.
+        artist_id (str): Spotify artist ID.
+        limit (int): Number of related artists to retrieve.
+
+    Returns:
+        list: A list of related artists with their names and Spotify URLs.
+    """
+    url = f'{SPOTIFY_API_BASE_URL}/artists/{artist_id}/related-artists'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        related_artists = data.get('artists', [])[:limit]
+        return [
+            {
+                'name': artist['name'],
+                'artist_link': artist['external_urls']['spotify']
+            }
+            for artist in related_artists
+        ]
+    else:
+        response.raise_for_status()
